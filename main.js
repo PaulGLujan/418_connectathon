02828e9549db632ed7760b7a2b5efb316c691fd4
delray <d.a.faskey@gmail.com>
2018-05-23 17:31:52 -0700

    2x2 Square Bonus functional

--- main.js
+++ main.js
@@ -27,4 +27,32 @@ var rowPiecePlace = 0;
 function getRowForPiece(piecesInColumn){
         rowPiecePlace = piecesInColumn -1;
     return rowPiecePlace;
-}
+};
+
+// 2x2 SQUARE BONUS
+function squareBonusCheck(boardArrayClick){
+    if (boardArray[row - 1][column + 1] === 'X'){
+        if (boardArray[row - 0][column + 1] === 'X' && boardArray[row - 1][column - 0] === 'X'){
+            console.log('SQUAREBONUS top right');
+            return;
+        };
+    };
+    if (boardArray[row-1][column-1] === 'X'){
+        if (boardArray[row-0][column-1] === 'X' && boardArray[row-1][column-0] === 'X'){
+            console.log('SQUAREBONUS top left');
+            return;
+        };
+    };
+    if (boardArray[row+1][column-1] === 'X'){
+        if (boardArray[row-0][column-1] === 'X' && boardArray[row+1][column-0] === 'X'){
+            console.log('SQUAREBONUS bottom left');
+            return;
+        };
+    };
+    if (boardArray[row+1][column+1] === 'X'){
+        if (boardArray[row-0][column+1] === 'X' && boardArray[row+1][column-0] === 'X'){
+            console.log('SQUAREBONUS bottom right');
+            return;
+        };
+    };
+};
